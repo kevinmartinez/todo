f903cb9a6d57dfcf753bf24479398ccc958fc273
Kevin <kevin@pixelkojan.se>
2016-12-25 11:55:26 +0100

    add precss to the postcss workflow

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var pxtorem = require('postcss-pxtorem');
 var shortColor = require('postcss-short-color');
+var precss = require('precss');
 
 gulp.task('default', function() {
   // place code for your default task here
@@ -10,6 +11,19 @@ gulp.task('default', function() {
 
 gulp.task('css', function () {
   return gulp.src('html/css/*.css')
-  .pipe(postcss([shortColor,pxtorem]))
+  .pipe(postcss([
+    precss,
+    shortColor,
+    pxtorem]))
   .pipe(gulp.dest('dist'));
 });
+
+// gulp.task('css', function () {
+//     return gulp.src('./css/src/*.css').pipe(
+//         postcss([
+//             require('precss')({ /* options */ })
+//         ])
+//     ).pipe(
+//         gulp.dest('./css')
+//     );
+// });
