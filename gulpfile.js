b5d906903f70fbd56a44e9d840654c569abbdc62
Kevin <kevin@pixelkojan.se>
2016-12-25 12:44:02 +0100

    add cssnano for minifying css and also make path for style.min.css file for dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
+var rename = require("gulp-rename");
 var postcss = require('gulp-postcss');
 var pxtorem = require('postcss-pxtorem');
 var shortColor = require('postcss-short-color');
 var precss = require('precss');
+var cssnano = require('cssnano');
 
 gulp.task('default', function() {
   // place code for your default task here
@@ -15,8 +17,17 @@ gulp.task('css', function () {
     precss,
     shortColor,
     pxtorem]))
-  .pipe(gulp.dest('dist'));
+  .pipe(gulp.dest('dist'))
 });
+
+gulp.task('minifycss', function() {
+  return gulp.src('dist/style.css')
+  .pipe(rename('dist/min/min.style.css'))
+  .pipe(postcss([cssnano]))
+  // rename via string
+  .pipe(gulp.dest('dist'))
+  
+})
 
 // gulp.task('css', function () {
 //     return gulp.src('./css/src/*.css').pipe(
