4cf538c3940369241a25ae09d2c0f8e43be4b237
Kevin <kevin@pixelkojan.se>
2017-04-28 16:16:37 +0200

    create a createItem function, creates li and append p and button. Add event listener to add item button

--- html/js/app.js
+++ html/js/app.js
@@ -9,6 +9,9 @@ console.log('program start');
 var addItemUl = document.querySelector('.add-item-ul');
 console.log(addItemUl);
 
+// Get todo-item container
+var todoItemUl = document.querySelector('.todo-item-ul');
+
 // Get add-item li
 var addItemLi = document.querySelector('.add-item');
 console.log(addItemLi)
@@ -16,3 +19,25 @@ console.log(addItemLi)
 // Get value from add-item input
 var addItemInput = addItemLi.firstElementChild.nextElementSibling;
 console.log('addItemValue:' + addItemInput.value);
+
+// Add item button
+var addButton = document.querySelector('.add-button');
+
+var createTodo = function() {
+    var todoLi = document.createElement('li');
+    var todoP = document.createElement('p');
+    var button = document.createElement('button');
+    
+    todoLi.appendChild(todoP);
+    todoLi.appendChild(button);
+    return todoLi;
+};
+
+// Click the button and output something
+addButton.addEventListener('click', function() {
+    var item = createTodo();
+    todoItemUl.appendChild(item);
+});
+
+// build new todo-item
+// create a  'create todo item' function
