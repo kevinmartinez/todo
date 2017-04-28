da058e60027c45d6a4fdc7c04c82365416bd2337
Kevin <kevin@pixelkojan.se>
2017-04-28 14:50:19 +0200

    With queryselector, select list containers. Get value from add item input

--- html/js/app.js
+++ html/js/app.js
@@ -1,53 +1,18 @@
-// 'use strict';
-console.debug('program begin');
-// add item to .todo-item-container
-    // when press button -add-button
-        // add li to .todo-citem-container ul
-// target add-button
-var addButton = document.querySelector('.add-button');
-var todoItemContainer = document.querySelector('.todo-item-container');
-var addItemInputEle = document.getElementById("add-item");
-var todoItemUl = document.querySelector('.todo-item-ul');
-// element variables
-var li = document.createElement('li');
-var label = document.createElement('label');
-var input = document.createElement('input');
-var button = document.createElement('button');
+'use strict';
+console.log('program start');
 
-// todo: reconsider functions
-// var createListItems = function() {
-//     var newItem = li;
-//     var newItemLabel = label;
-//     var newItemInput = input;
-// }
+// User writes a todo-item
+// User press add button to add
+// todo-item goes into todo-item container
 
-//  var addListItem = function () {
-//     console.log('hello from event');
-//     var newItem = li;
-//     var newItemLabel = label;
-//     newItemLabel.setAttribute('for', 'add-item');
-//     newItemLabel.innerHTML = 'done?';
-//     newItem.appendChild(newItemLabel);
-    
-//     // var text = addItemInputEle.value;
-//     var newItemInput = input;
-//     newItemInput.setAttribute('type', 'text');
-//     newItem.appendChild(newItemInput);
-//     var newItemButton = button;
-//     //newItemButton.classList.add('add-button');
-//     newItemButton.setAttribute('type', 'checkbox');
-//     newItem.appendChild(newItemButton);
-//     // label.setAttribute('for', 'add-item');
-   
-//     // newItemInput.setAttribute('placeholder', text); 
-//     todoItemUl.appendChild(newItem, todoItemUl)
-// }
+// Get the add item container
+var addItemUl = document.querySelector('.add-item-ul');
+console.log(addItemUl);
 
-// addButton.addEventListener('click', addListItem());
+// Get add-item li
+var addItemLi = document.querySelector('.add-item');
+console.log(addItemLi)
 
-
-
-
-
-
-
+// Get value from add-item input
+var addItemInput = addItemLi.firstElementChild.nextElementSibling;
+console.log('addItemValue:' + addItemInput.value);
